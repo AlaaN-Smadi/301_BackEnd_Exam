9d82f38ced709345db45fe6b48279d62fa8267f1
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-08-03 11:28:04 +0300

    Finish all backEnd Codes

---
+++ server.js
@@ -0,0 +1,201 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const {default: axios} = require('axios');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost:27017/finalExam', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`Listening on PORT ${PORT}`)
+})
+
+
+////   our  Code 
+
+////   Mongoose Sechamas and module 
+
+const itemSchema = new Schema({
+    name: String,
+    image: String
+})
+const userSchema = new Schema({
+    email: String,
+    favItems: [itemSchema]
+})
+
+const userModel = new mongoose.model('User', userSchema)
+
+
+////   Seeding Function
+
+function seedFunction(){
+
+    ///  Razan  User
+    const razan = new userModel({
+        email: 'quraanrazan282@gmail.com',
+        favItems:[
+            {
+                name:'Black',
+                image:"http://www.colourlovers.com/img/000000/100/100/Black.png"
+            },
+            {
+                name:'serenity is . . .',
+                image:"http://www.colourlovers.com/img/ADD8C7/100/100/serenity_is_._._..png"
+            }
+        ]
+    })
+
+    ///   user  for  alaaads27@gmail.com
+    const alaa1 = new userModel({
+        email: 'alaaads27@gmail.com',
+        favItems:[
+            {
+                name:'Black',
+                image:"http://www.colourlovers.com/img/000000/100/100/Black.png"
+            },
+            {
+                name:'serenity is . . .',
+                image:"http://www.colourlovers.com/img/ADD8C7/100/100/serenity_is_._._..png"
+            }
+        ]
+    })
+
+    ///   user for alaasmadi1010@gmail.com
+    const alaa2 = new userModel({
+        email: 'alaasmadi1010@gmail.com',
+        favItems:[
+            {
+                name:'Black',
+                image:"http://www.colourlovers.com/img/000000/100/100/Black.png"
+            },
+            {
+                name:'serenity is . . .',
+                image:"http://www.colourlovers.com/img/ADD8C7/100/100/serenity_is_._._..png"
+            }
+        ]
+    })
+
+    razan.save();
+    alaa1.save();
+    alaa2.save();
+
+}
+
+
+////    calling seeding function
+
+// seedFunction()
+////  Getting Data from API  =>
+
+app.get('/getApiData', async(req, res) =>{
+    const apiUrl = 'https://ltuc-asac-api.herokuapp.com/allColorData'
+    const data = await axios.get(apiUrl)
+    console.log(data.data)
+
+    res.send(data.data)
+})
+
+
+
+
+////   Getting Fav  Items
+
+app.get('/myFavItems', (req,res)=>{
+    const userEmail = req.query.userEmail
+
+
+    userModel.find({email: 'alaasmadi1010@gmail.com'}, function(error, userInfo){
+        if(error){
+            res.send('Data not Found')
+        }else{
+            console.log(userInfo[0].favItems);
+            res.send(userInfo[0].favItems)
+        }
+    })
+
+})
+
+///   Add  to my Fav =>
+
+app.get('/addNewFav', (req,res)=>{
+    const userEmail = req.query.userEmail
+    const itemName = req.query.itemName
+    const itemImage = req.query.itemImage
+
+    userModel.find({email: userEmail}, function(error, userInfo){
+        if(error){
+            res.send('Data not Found')
+        }else{
+            
+            const newObj = {
+                name: itemName,
+                image: itemImage
+            }
+
+            userInfo[0].favItems.push(newObj)
+            userInfo[0].save();
+
+            res.send(userInfo[0].favItems)
+        }
+    })
+})
+
+
+
+/////    Update  My  Fave =>
+
+app.get('/updateMyFave', (req,res)=>{
+    const userEmail = req.query.userEmail
+    const itemName = req.query.itemName
+    const itemImage = req.query.itemImage
+    const index = req.query.index
+
+    userModel.find({email: userEmail}, function(error, userInfo){
+        if(error){
+            res.send('Data not Found')
+        }else{
+            
+
+            userInfo[0].favItems[index].name = itemName
+            userInfo[0].favItems[index].image = itemImage
+
+            userInfo[0].save();
+
+            res.send(userInfo[0].favItems)
+        }
+    })
+})
+
+
+
+/////    Delete My  Item  =>
+
+app.get('/deleteMyItem', (req,res)=>{
+    const userEmail = req.query.userEmail
+    const index = req.query.index
+
+
+    userModel.find({email: userEmail}, function(error, userInfo){
+        if(error){
+            res.send('Data not Found')
+        }else{
+           
+            userInfo[0].favItems.splice(index, 1)
+
+            userInfo[0].save();
+
+            res.send(userInfo[0].favItems)
+        }
+    })
+})
